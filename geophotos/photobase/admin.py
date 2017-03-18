807df3c549df20f4321f77f686adb93fa89ed968
Norton Pengra <npengra317@gmail.com>
2017-03-18 13:58:49 -0700

    create simple admin page

--- geophotos/photobase/admin.py
+++ geophotos/photobase/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from photobase.models import Location, Coords, SitePlan, SiteDoc, Image
 
 # Register your models here.
+
+admin.site.register(Location)
+admin.site.register(Coords)
+admin.site.register(SitePlan)
+admin.site.register(SiteDoc)
+admin.site.register(Image)
