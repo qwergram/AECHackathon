7166fb7567411d8563b92ece2c0af0ac04705c9a
Norton Pengra <npengra317@gmail.com>
2017-03-18 15:12:22 -0700

    update views to use post

--- geophotos/photobase/views.py
+++ geophotos/photobase/views.py
@@ -30,3 +30,28 @@ class LocationView(APIView):
         locations = Location.objects.all()
         serializer = LocationSerializer(locations, many=True)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = LocationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CoordsView(APIView):
+
+    def get_queryset(self):
+        return Coords.objects.all()
+
+    def get(self, request, format=None):
+        coords = Coords.objects.all()
+        serializer = CoordsSerializer(coords, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = CoordsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST) 
