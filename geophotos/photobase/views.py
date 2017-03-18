7e7571dcd968223aff5851fe9cae43ff99cea250
Norton Pengra <npengra317@gmail.com>
2017-03-18 14:57:24 -0700

    create serializer view for location

--- geophotos/photobase/views.py
+++ geophotos/photobase/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+# from django.shortcuts import render
+# from django.views.generic import TemplateView
+
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from photobase.models import Location, Coords, SitePlan, SiteDoc, Image
+from photobase.serializers import (
+    LocationSerializer, 
+    CoordsSerializer, 
+    SitePlanSerializer, 
+    SiteDocSerializer,
+    ImageSerializer
+)
+
+# syntatic sugars
+GET = 'GET'
+POST = 'POST'
 
 # Create your views here.
 
-class FlagShip(TemplateView):
-    template_name = "photobase/index.html"
+class LocationView(APIView):
+
+    def get_queryset(self):
+        return Location.objects.all()
+
+    def get(self, request, format=None):
+        locations = Location.objects.all()
+        serializer = LocationSerializer(locations, many=True)
+        return Response(serializer.data)
