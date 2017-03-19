508d96e6e9675ca07b0566a92498604f79ac61c3
Norton Pengra <npengra317@gmail.com>
2017-03-18 17:49:49 -0700

    update import statements

--- geophotos/photobase/views.py
+++ geophotos/photobase/views.py
@@ -6,7 +6,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from photobase.models import Location, Coords, SitePlan, SiteDoc, Image
+from photobase.models import SiteDoc, Image
 from photobase.serializers import (
     SiteDocSerializer,
     ImageSerializer
