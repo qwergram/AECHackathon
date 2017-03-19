a6e52c626c10ee9431d6499fe388874f9b839a2a
Norton Pengra <npengra317@gmail.com>
2017-03-18 17:48:33 -0700

    update api views

--- geophotos/photobase/views.py
+++ geophotos/photobase/views.py
@@ -8,72 +8,11 @@ from rest_framework import status
 
 from photobase.models import Location, Coords, SitePlan, SiteDoc, Image
 from photobase.serializers import (
-    LocationSerializer, 
-    CoordsSerializer, 
-    SitePlanSerializer, 
     SiteDocSerializer,
     ImageSerializer
 )
 
-# syntatic sugars
-GET = 'GET'
-POST = 'POST'
-
 # Create your views here.
-
-class LocationView(APIView):
-
-    def get_queryset(self):
-        return Location.objects.all()
-
-    def get(self, request, format=None):
-        locations = self.get_queryset()
-        serializer = LocationSerializer(locations, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = LocationSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
-
-
-class CoordsView(APIView):
-
-    def get_queryset(self):
-        return Coords.objects.all()
-
-    def get(self, request, format=None):
-        coords = self.get_queryset()
-        serializer = CoordsSerializer(coords, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = CoordsSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
-
-
-class SitePlanView(APIView):
-    
-    def get_queryset(self):
-        return SitePlan.objects.all()
-
-    def get(self, request, format=None):
-        siteplans = self.get_queryset()
-        serializer = SitePlanSerializer(siteplans, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = SitePlanSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
-
 
 class SiteDocView(APIView):
     
